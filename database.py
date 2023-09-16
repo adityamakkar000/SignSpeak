b0076402840d04e2ee2e617769155abe51886574
Blueishfiend692 <aditymakkar000@gmail.com>
2023-09-16 11:23:19 -0400

    intial databse

---
+++ database.py
@@ -0,0 +1,43 @@
+import serial
+import pymongo
+import os
+
+# connect and setup mongodb database
+client = pymongo.MongoClient("mongodb+srv://blueishfiend692:EBqcMyVksJPcK2QA@cluster0.so0ju7f.mongodb.net/")
+db = client['cluster0']
+collection = db[('new_letters')]
+
+# average to smooth out data
+averageRun = 10
+
+def insert_data(data):
+  if collection.count_documents({"word": data["word"]}) == 0: # if word does not exist in database
+    collection.insert_one(data)
+
+ser = serial.Serial('COM3', 9600)
+
+final_arr = []
+while True:
+  try:
+    res_arr = [0,0,0,0,0]
+    for i in range(0, averageRun):
+      line = ser.readline().decode('utf-8').strip()  # Read a line of text from the serial port
+      if line:  # Check if the line is not empty
+        a = line
+            # print(a)
+            #covert string to integer array
+        arr = list(map(int, a.split(' ')))
+        for i in range(0,5):
+          res_arr[i] += arr[i]
+    for i in range(0,5):
+      res_arr[i] = res_arr[i]/averageRun
+    print(res_arr)
+    final_arr.append(res_arr)
+  except KeyboardInterrupt:
+    word = input("Enter a word: ")
+    data = {"word": word, "hand": final_arr}
+    insert_data(data)
+    key = 'b'
+    break
+
+
---
+++ nearstneighbor.py
@@ -0,0 +1,76 @@
+import serial
+import pymongo
+import os
+import math
+
+
+client = pymongo.MongoClient("mongodb+srv://blueishfiend692:EBqcMyVksJPcK2QA@cluster0.so0ju7f.mongodb.net/")
+db = client['cluster0']
+collection = db[('new_letters')]
+
+averageRun = 10
+ser = serial.Serial('COM3', 9600)
+
+def get_letter_dict():
+    cursor = collection.find()
+    data = list(cursor)
+    letters = [entry["word"] for entry in data]
+    resistance_values = [entry["hand"] for entry in data]
+    letter_dict = dict(zip(letters, resistance_values))
+    return letter_dict
+
+letter_dictionary = get_letter_dict()
+
+wait_input = input()
+
+final_arr = []
+while True:
+  try:
+    res_arr = [0,0,0,0,0]
+    for i in range(0, averageRun):
+      line = ser.readline().decode('utf-8').strip()  # Read a line of text from the serial port
+      if line:  # Check if the line is not empty
+        a = line
+            # print(a)
+            #covert string to integer array
+        arr = list(map(int, a.split(' ')))
+        for i in range(0,5):
+          res_arr[i] += arr[i]
+    for i in range(0,5):
+      res_arr[i] = res_arr[i]/averageRun
+    print(res_arr)
+    final_arr.append(res_arr)
+  except KeyboardInterrupt:
+    break
+
+distance_arr = []
+
+for i in range(0,len(letter_dictionary)):
+  distance_arr.append(0)
+
+
+power = 3
+
+for i in range(0, len(final_arr)):
+  for j in letter_dictionary:
+    letter = letter_dictionary[j]
+    time_arr = letter[i]
+    distance_arr[list(letter_dictionary).index(j)] += pow(
+        (pow(abs(final_arr[i][0] - time_arr[0]),power)
+      + pow(abs(final_arr[i][1] - time_arr[1]),power)
+      + pow(abs(final_arr[i][2] - time_arr[2]),power)
+      + pow(abs(final_arr[i][3] - time_arr[3]),power)
+      + pow(abs(final_arr[i][4] - time_arr[4]),power)), (1/power)
+    )
+
+
+min_dis = 1000000000000
+min_index = -1
+
+for i in range(0,len(distance_arr)):
+  if(distance_arr[i] < min_dis):
+    min_dis = distance_arr[i]
+    min_index = i
+
+print(min_index)
+print(distance_arr)
