7953fae51b1b861903cd2728b320aba48222593f
Aarav Patel <Aarav@MacBook-Pro-3.local>
2023-09-16 17:38:21 -0400

    data

--- stop.py
+++ stop.py
@@ -1,7 +1,5 @@
 import serial
 import pymongo
-import os
-import math
 from Text import TextToSpeech
 
 client = pymongo.MongoClient("mongodb+srv://blueishfiend692:EBqcMyVksJPcK2QA@cluster0.so0ju7f.mongodb.net/")
@@ -21,7 +19,7 @@ ser = serial.Serial('COM5', 9600)
 def get_letter_dict():
   cursor = collection.find()
   data = list(cursor)
-  letters = [entry["word"] for entry in data]c b
+  letters = [entry["word"] for entry in data]
   resistance_values = [entry["hand"] for entry in data]
   letter_dict = dict(zip(letters, resistance_values))
   return letter_dict
@@ -29,12 +27,48 @@ def get_letter_dict():
 letter_dictionary = get_letter_dict()
 
 
-state = 0
+state = False
+stop_amount = 20
 
 while True:
-  line = ser.readline().decode('utf-8').strip()
-  count = 0:
 
-  while count < 0: 
+  while state == False: 
+
+    line = ser.readline().decode('utf-8').strip()
+    a = line
+    arr = list(map(int,a.split(' ')))
+    sum = 0
+    for i in arr:
+      sum += i 
+    if sum < 9700:
+      state = True 
+
+  
+  while state == True: 
+    count = 0
+    final_arr = []
+    while count < stop_amount: 
+      line = ser.readline().decode('utf-8').strip()
+      a = line
+      arr = list(map(int,a.split(' ')))
+      sum = 0
+      for i in arr:
+        sum += i 
+      if sum >= 9700:
+        count += 1
+      else: 
+        final_arr.append(arr)
+
+    # use prediction then 
+    #  
+    print("stop")  
+    state = False     
+
+
+
+
+   
+
+    
 
 
