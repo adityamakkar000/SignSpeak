e69a0f2dd5a9066b41c9b5d68eb55a4469dcea17
Blueishfiend692 <aditymakkar000@gmail.com>
2023-12-20 21:33:53 -0500

    testing

--- EE/EErnn.py
+++ EE/EErnn.py
@@ -11,8 +11,6 @@ import keras
 
 load_dotenv()
 
-
-
 client = pymongo.MongoClient(os.getenv("MONGO_URI"))
 db = client['cluster0']
 collection = [
@@ -52,31 +50,30 @@ batch_size = 64
 epochs = 1000
 trials = 10
 
-for i in range(trials):
-    m1 = researchModel("SimpleRNN", 1, batch_size, epochs, x, y)
-    results1 = m1.evaluate()
-    print(results1)
-    collection[0].insert_one({'model': 'SIMPLE_RNN_1_layer', 'loss': results1[0], 'accuracy': results1[1], 'Macro F1-Score': results1[2]})
+# for i in range(trials):
+#     m1 = researchModel("SimpleRNN", 1, batch_size, epochs, x, y)
+#     results1 = m1.evaluate()
+#     collection[0].insert_one({'model': 'SIMPLE_RNN_1_layer', 'loss': results1[0], 'accuracy': results1[1], 'Macro F1-Score': results1[2]})
 
-for i in range(trials):
-    m2 = researchModel("SimpleRNN", 2, batch_size, epochs, x, y)
-    results2 = m2.evaluate()
-    collection[1].insert_one({'model': 'SIMPLE_RNN_2_layer', 'loss': results2[0], 'accuracy': results2[1], 'Macro F1-Score': results2[2]})
+# for i in range(trials):
+#     m2 = researchModel("SimpleRNN", 2, batch_size, epochs, x, y)
+#     results2 = m2.evaluate()
+#     collection[1].insert_one({'model': 'SIMPLE_RNN_2_layer', 'loss': results2[0], 'accuracy': results2[1], 'Macro F1-Score': results2[2]})
 
-for i in range(trials):
-    m3 = researchModel("GRU", 1, batch_size, epochs, x, y)
-    results3 = m3.evaluate()
-    collection[2].insert_one({'model': 'GRU_RNN_1_layer', 'loss': results3[0], 'accuracy': results3[1], 'Macro F1-Score': results3[2]})
+# for i in range(trials):
+#     m3 = researchModel("GRU", 1, batch_size, epochs, x, y)
+#     results3 = m3.evaluate()
+#     collection[2].insert_one({'model': 'GRU_RNN_1_layer', 'loss': results3[0], 'accuracy': results3[1], 'Macro F1-Score': results3[2]})
 
-for i in range(trials):
-    m4 = researchModel("GRU", 2, batch_size, epochs, x, y)
-    results4 = m4.evaluate()
-    collection[3].insert_one({'model': 'GRU_RNN_2_layer', 'loss': results4[0], 'accuracy': results4[1], 'Macro F1-Score': results4[2]})
+# for i in range(trials):
+#     m4 = researchModel("GRU", 2, batch_size, epochs, x, y)
+#     results4 = m4.evaluate()
+#     collection[3].insert_one({'model': 'GRU_RNN_2_layer', 'loss': results4[0], 'accuracy': results4[1], 'Macro F1-Score': results4[2]})
 
-for i in range(trials):
-    m5 = researchModel("LSTM", 1, batch_size, epochs, x, y)
-    results5 = m5.evaluate()
-    collection[4].insert_one({'model': 'LSTM_RNN_1_layer', 'loss': results5[0], 'accuracy': results5[1], 'Macro F1-Score': results5[2]})
+# for i in range(trials):
+    # m5 = researchModel("LSTM", 1, batch_size, epochs, x, y)
+    # results5 = m5.evaluate()
+    # collection[4].insert_one({'model': 'LSTM_RNN_1_layer', 'loss': results5[0], 'accuracy': results5[1], 'Macro F1-Score': results5[2]})
 
 for i in range(trials):
     m6 = researchModel("LSTM", 2, batch_size, epochs, x, y)
