37f63194325e097b6a1b20139a1350f7cc78414f
Blueishfiend692 <aditymakkar000@gmail.com>
2024-03-28 14:19:38 -0400

    add RNN in pytorch

--- v2.0/models/RNN.py
+++ v2.0/models/RNN.py
@@ -1,6 +1,10 @@
+from torcheval.metrics.functional import multiclass_f1_score
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import pandas
 import numpy as np
+
 
 # upload data
 def data_upload(model, collection):
@@ -41,4 +45,67 @@ y = torch.tensor([encode(s) for i,s in enumerate(y)])
 #prepare data
 x = torch.tensor(data.iloc[:,2:].to_numpy())
 x = torch.nan_to_num(x)
+
+
+# prepare classes
+data = pandas.read_csv('./Data.csv')
+
+
+y = data['word']
+stoi  = {s:i for i,s in enumerate(sorted(set(y)))}
+encode = lambda s: stoi[s]
+y = torch.tensor([encode(s) for i,s in enumerate(y)])
+print(y)
+
+# y = F.one_hot((y.view(y.shape[0], 1)),10)
+
+x = data.iloc[:,2:52].to_numpy()
+print(data.iloc[:,2:])
+x = torch.tensor(x, dtype = torch.float32)
+x = torch.nan_to_num(x)
+x = x.view(x.shape[0], -1, 5)
+print(x[0,:5])
+
+torch.manual_seed(1)
+class LSTMStacked(nn.Module):
+
+  def __init__(self, batch_first=True, layers=1, dense_layer=False):
+    super().__init__()
+    self.layer1 = nn.LSTM(5, 64, num_layers = 2, batch_first = batch_first)
+    self.layer2 = nn.Linear(64,64)
+    self.layer3 = nn.Tanh()
+    self.layer4 = nn.Linear(64,10)
+
+  def forward(self, x):
+    self.inter_output, self.inter_h = self.layer1(x)
+    self.layer2out = self.layer2(self.inter_h[-1])
+    self.layer3out = self.layer2out.tanh()
+    self.out = self.layer4(self.layer3out)
+
+    return self.out
+
+model1 = LSTMStacked(True,1)
+model2 = LSTMStacked()
+
+output1 = model1(x)
+
+print(output1.shape)
+
+epochs = 10000
+optimizer = torch.optim.AdamW(model1.parameters(),lr=1e-3)
+
+for i in range(epochs):
+
+  x_output = model1(x)
+  loss = F.cross_entropy(x_output,y)
+
+  optimizer.zero_grad(set_to_none=True)
+  loss.backward()
+  f1 = multiclass_f1_score(x_output, y, num_classes=10, average='macro')
+
+  if(i % 100 == 0):
+    print(i, " ",loss.item(), " ", f1)
+
+  optimizer.step()
+
 
